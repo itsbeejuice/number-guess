75ccca1f6721756a550892209bf528086048b9d8
Bee (Aka. Outlaw) <77043377+itsbeejuice@users.noreply.github.com>
2021-01-21 11:54:36 -0800

    Add files via upload

---
+++ guessing game (UPDATED).py
@@ -0,0 +1,35 @@
+import random
+
+name=input("What is your name contestant? ")
+print("Welcome",name,"The goal of this game is to simply guess and I'll tell you if you've won or not!")
+lvl=input("We have 3 levels\nbeginner (numbers 1-10)\nmedium (numbers 1-50)\nand\nhard (numbers 1-100)\nPlease choose here:")
+
+if lvl=="beginner":
+    x=random.randrange(1,10)
+    number=int(input("Enter your choice: "))
+    if(number==x):
+        print("WINNERRRRRRRRR")
+    elif(number>x):
+        print("Your number is over the guessed number, sorry. Better luck next time!")
+    elif(number<x):
+        print("Your number was less than the chosen number sorry, better luck next time!")
+    #take loop and look it for lines 11-16 in future updates
+elif lvl=="medium":
+    x=random.randrange(1,50)
+    number2=int(input("Enter your number: "))
+    if (number2==x):
+        print("WINNERRRRRRRRRRRR")
+    elif(number2>x):
+        print("Your number was larger than the guessed number, sorry. Better luck next time!")
+    elif(number2<x):
+        print("Your number was less than the chosen number my apologies!")
+        #loop this smh, reference the hangman game for more helpk 
+elif lvl=="hard":
+    x=random.randrange(1,100)
+    number3=int(input("Enter your guess: "))
+    if (number3==x):
+        print("WINNERRRRRR")
+    elif (number3>x):
+        print("Your number was larger than our number, better luck next time!")
+    elif (number3<x):
+        print("Your number was smaller than our guessed number, better luck next time!")
